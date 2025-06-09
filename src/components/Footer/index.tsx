26ca4d48d1534b809b74e88cdb299da8ebcba8e5
Andreas Fredriksson <afredriksson89@gmail.com>
2025-06-09 10:59:35 +0200

    fix: update footer attribution text from "gjord av" to "upprättad av"

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -39,7 +39,7 @@ export function Footer() {
           <div className=" mt-8 text-sm">
             <p className="text-sm text-muted-foreground">
               © {currentYear}
-              <span> gjord av</span>
+              <span> upprättad av</span>
               <span className="text-orange-600 font-bold"> AAL</span>
               goritm
               <span className="text-orange-600 font-bold">()</span>. Alla
